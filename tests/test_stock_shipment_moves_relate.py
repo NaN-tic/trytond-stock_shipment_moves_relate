7b06872ad14b6cb4111afe895dfac434e5f331df
resteve <resteve@zikzakmedia.com>
2015-12-01 00:59:00 +0100

    Tests

--- tests/test_stock_shipment_moves_relate.py
+++ tests/test_stock_shipment_moves_relate.py
@@ -1,25 +1,14 @@
-#!/usr/bin/env python
-# This file is part stock_shipment_moves_relate module for Tryton.
-# The COPYRIGHT file at the top level of this repository contains
-# the full copyright notices and license terms.
+# This file is part of the stock_shipment_moves_relate module for Tryton.
+# The COPYRIGHT file at the top level of this repository contains the full
+# copyright notices and license terms.
 import unittest
 import trytond.tests.test_tryton
-from trytond.tests.test_tryton import test_depends
+from trytond.tests.test_tryton import ModuleTestCase
 
 
-class StockShipmentMovesRelateTestCase(unittest.TestCase):
+class StockShipmentMovesRelateTestCase(ModuleTestCase):
     'Test Stock Shipment Moves Relate module'
-
-    def setUp(self):
-        trytond.tests.test_tryton.install_module('stock_shipment_moves_relate')
-
-    def test0005views(self):
-        'Test views'
-        test_view('stock_shipment_moves_relate')
-
-    def test0006depends(self):
-        'Test depends'
-        test_depends()
+    module = 'stock_shipment_moves_relate'
 
 
 def suite():
@@ -27,6 +16,3 @@ def suite():
     suite.addTests(unittest.TestLoader().loadTestsFromTestCase(
         StockShipmentMovesRelateTestCase))
     return suite
-
-if __name__ == '__main__':
-    unittest.TextTestRunner(verbosity=2).run(suite())
